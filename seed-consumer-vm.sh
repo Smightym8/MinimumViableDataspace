1d1bc09c9783c9f415fc37ca4d8f57d80e449c8c
Michael Spiegel <michaelspiegel@live.at>
2025-03-26 16:22:08 +0100

    fix jwts, fix docker compose, fix participant context id in seed scripts
    Co-authored-by: Marco Prescher <mpr8243@students.fhv.at>

--- seed-consumer-vm.sh
+++ seed-consumer-vm.sh
@@ -14,7 +14,7 @@ DATA_CONSUMER=$(jq -n --arg url "$CONSUMER_CONTROLPLANE_SERVICE_URL" --arg ihurl
            "serviceEndpoints":[
              {
                 "type": "CredentialService",
-                "serviceEndpoint": "\($ihurl)/api/presentation/v1/participants/ZGlkOndlYjpjb25zdW1lci1pZGVudGl0eWh1YiUzQTcwODM6Y29uc3VtZXI=",
+                "serviceEndpoint": "\($ihurl)/api/presentation/v1/participants/ZGlkOndlYjoxOTIuMTY4LjEuMTAxJTNBNzA4Mzpjb25zdW1lcg==",
                 "id": "consumer-credentialservice-1"
              },
              {
